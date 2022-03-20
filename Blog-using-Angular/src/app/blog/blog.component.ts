327550e54e40979144eaf170ae0db42a50b9aa75
soham <thakersoham3@gmail.com>
2022-03-20 16:27:46 -0400

    initial commit

---
+++ Blog-using-Angular/src/app/blog/blog.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import blogData from '../blogData.json';
+import { BlogPost } from '../BlogPost';
+
+@Component({
+  selector: 'app-blog',
+  templateUrl: './blog.component.html',
+  styleUrls: ['./blog.component.css'],
+})
+
+//fetches data from api in future
+export class BlogComponent implements OnInit {
+  blogPosts: Array<BlogPost> = blogData;
+  constructor() {}
+
+  ngOnInit(): void {}
+}
---
+++ Blog-using-Angular/src/app/latest-posts/latest-posts.component.ts
@@ -0,0 +1,17 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { BlogPost } from '../BlogPost';
+
+@Component({
+  selector: 'app-latest-posts',
+  templateUrl: './latest-posts.component.html',
+  styleUrls: ['./latest-posts.component.css'],
+})
+export class LatestPostsComponent implements OnInit {
+  @Input() posts: Array<BlogPost>;
+
+  constructor() {
+    this.posts = new Array<BlogPost>();
+  }
+
+  ngOnInit(): void {}
+}
