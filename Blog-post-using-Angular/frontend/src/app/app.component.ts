38524f05bbc9f6724d28fcd16d008a247063ef5d
soham <thakersoham3@gmail.com>
2022-03-29 14:56:44 -0400

    updated files

--- Blog-post-using-Angular/frontend/src/app/app.component.ts
+++ Blog-post-using-Angular/frontend/src/app/app.component.ts
@@ -1,12 +1,12 @@
 /*********************************************************************************
- * BTI425 – Assignment 03
+ * BTI425 – Assignment 04
  * I declare that this assignment is my own work in accordance with Seneca  Academic Policy.  No part of this
  * assignment has been copied manually or electronically from any other source (including web sites) or
  * distributed to other students.
  * Name: Soham Thaker
  * Student ID: 011-748-159
  * Date: 15/03/2022
- * Heroku Link: https://mighty-sam.herokuapp.com
+ * Heroku Link: https://blog-post-app-frontend.herokuapp.com
  * ********************************************************************************/
 //https://blog-post-app-backend.herokuapp.com/
 
